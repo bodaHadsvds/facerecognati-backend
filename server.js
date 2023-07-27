905cbf5138dd5362dc1c74c70a979ad6f7c11552
Abdelrahman mustafa <92968146+bodaHadsvds@users.noreply.github.com>
2023-07-27 11:49:40 +0200

    meeee

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post("/signin" ,(req,res  )=>{signin.handelsignin(req,res ,db,bcrypt )} )
 
 
     
-let server = app.listen( 3000, ()=>{
+ app.listen( process.env.PORT ||3000, ()=>{
     console.log("app is running on port 3000" )
 })
 
